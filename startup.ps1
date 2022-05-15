c09f1fd1e6f94a4ce98e223bb48dfe974461a9fa
bottycode <54848714+bottycode@users.noreply.github.com>
2022-05-15 13:28:58 +0800

    Execute rc.local as root

--- startup.ps1
+++ startup.ps1
@@ -1,4 +1,4 @@
 $distros = (wsl.exe --list --quiet | Out-String) -replace "`0","" -replace "(`r`n)+","`n"
 foreach ($distro in $distros.Trim().Split()) {
-	wsl.exe -d $distro -- bash -c "[ -x /etc/rc.local ] && /etc/rc.local start"
+	wsl.exe -d $distro -u root -- bash -c "[ -x /etc/rc.local ] && /etc/rc.local start"
 }
